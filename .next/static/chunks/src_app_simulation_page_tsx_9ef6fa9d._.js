c784381df6854c174065a6139a06a78387acd5d9
gabriel.q.omar <gabriel.q.omar@gmail.com>
2025-10-05 22:58:49 +0000

    Remove .env and .next from git tracking

--- .next/static/chunks/src_app_simulation_page_tsx_9ef6fa9d._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/src_app_simulation_page_tsx_2f958e58._.js"
-],
-    source: "dynamic"
-});
